5d6aa09e874d17bab8411b49ebf199d3ea10b458
AbramSin17 <abrahamseputra@gmail.com>
2025-01-18 22:30:47 +0700

    keterkaitan mata kuliah

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-          Ankala Dokumen
+          Ankala Journey
       </h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        berisi setiap hasil dokumentasi mengenai Ankala Projek
+        Sebuah penyedia layanan jasa tourguide bagi para wisatawan
       </p>
       <div className="flex items-center gap-5">
         <Link
